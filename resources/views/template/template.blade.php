f36e361c4622b79a8a9a4206985a92d3272b2c8e
RFJST4REAL <rifianfernando19@gmail.com>
2023-05-09 08:36:39 +0700

    feat(template): add template for tailwind config

---
+++ resources/views/template/template.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <!-- Meta -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Vite css -->
+    @vite(['resources/css/app.css'])
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
+    <!-- Title -->
+    <title>FineAnce @yield('title')</title>
+
+    <!-- Yield -->
+    @yield('head')
+</head>
+
+@yield('body')
+
+</html>
