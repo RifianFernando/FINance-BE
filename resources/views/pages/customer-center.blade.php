46e3f0fab8b8d53729a343c1508678e535ce3414
Anselmus Kevin Purnomo <anselmus.purnomo@binus.ac.id>
2023-05-28 13:05:23 +0700

    .

--- resources/views/pages/customer-center.blade.php
+++ resources/views/pages/customer-center.blade.php
@@ -33,19 +33,19 @@
 
                 <div class="w-full flex justify-center mt-4 bg-gray-300 p-4">
                     <div class="grid grid-cols-3 w-5/5 gap-6">
-                        <a href="https://outlook.office.com/" class="flex rounded-xl text-lg flex-col items-center bg-white drop-shadow-subs-card py-8 px-10 hover:bg-gray-200 duration-300 md:px-3">
+                        <a href="https://www.skype.com/" class="flex rounded-xl text-lg flex-col items-center bg-white drop-shadow-subs-card py-8 px-10 hover:bg-gray-200 duration-300 md:px-3">
                             <img class="w-[48px] h-[48px] " src="{{asset('assets/customer-center/Call.png')}}">
                             <p class="font-poppins text-[20px] text-center font-bold pt-4 ">PHONE</p>
                             <hr class="h-[1px] border-0 w-[30%] bg-black mt-4">
                             <p class="font-castoro text-center mt-4 lg:text-[18px]">+62 812 3456 7890</p>
                         </a>
-                        <a href="https://www.skype.com/" class="flex rounded-xl text-lg flex-col items-center bg-white drop-shadow-subs-card py-8 px-10 hover:bg-gray-200 duration-300 md:px-3">
+                        <a href="https://www.reddit.com/" class="flex rounded-xl text-lg flex-col items-center bg-white drop-shadow-subs-card py-8 px-10 hover:bg-gray-200 duration-300 md:px-3">
                             <img class="w-[50px] h-[50px] mx-2" src="{{asset('assets/customer-center/FAQ.png')}}">
                             <p class="font-poppins text-[20px] text-center font-bold pt-4">FAQs</p>
                             <hr class="h-[1px] border-0 w-[30%] bg-black mt-4">
                             <h2 class="text-xl border-2 px-3 bg-fin-blue  text-center text-white py-2 my-4 rounded-md hover:border-fin-blue hover:bg-gray-100 hover:text-fin-blue duration-500 ease-in-out">Learn More</h2>
                         </a>
-                        <a href="https://www.skype.com/" class="flex rounded-xl text-lg flex-col items-center bg-white drop-shadow-subs-card py-8 px-10 hover:bg-gray-200 duration-300 md:px-3">
+                        <a href="https://outlook.office.com/" class="flex rounded-xl text-lg flex-col items-center bg-white drop-shadow-subs-card py-8 px-10 hover:bg-gray-200 duration-300 md:px-3">
                             <img class="w-[55px] h-[48px] mx-2" src="{{asset('assets/customer-center/mail.png')}}">
                             <p class="font-poppins text-[20px] text-center font-bold pt-4">EMAIL</p>
                             <hr class="h-[1px] border-0 w-[30%] bg-black mt-4">
