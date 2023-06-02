7f25c99a254ea10f6e1e6214f93bc43279991a6f
WTaroki <william.tanjaya@binus.ac.id>
2023-06-02 21:14:02 +0700

    Added More Design in Dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -35,16 +35,16 @@
 
 
                     <!-- News Image Slider-->
-                    <div class="flex  bg-white drop-shadow-subs-card rounded-md col-span-2">
-                        <div class=" flex flex-row" id="image-slide">
-                            <img class="w-full h-full fixed" src="{{asset('assets/customer-center/pic.png')}}">
-                            <img class="w-full h-full fixed" src="{{asset('assets/register/illustration-signup.jpg')}}">
-                            <img class="w-full h-full fixed" src="{{asset('assets/customer-center/pic.png')}}">
+                    <div class="flex  bg-white drop-shadow-subs-card rounded-md col-span-2 overflow-hidden">
+                        <div class="ImageSlider flex flex-row">
+                            <img class="w-full h-full fixed" src="{{asset('assets/dashboard/dummy1.jpeg')}}">
+                            <img class="w-full h-full fixed" src="{{asset('assets/dashboard/dummy2.jpeg')}}">
+                            <img class="w-full h-full fixed" src="{{asset('assets/dashboard/dummy3.jpeg')}}">
                         </div>
-                        <div class="absolute top-0 right-2" id="radio-list">
-                            <input type="radio" id="Radio1" checked>
-                            <input type="radio" id="Radio2">
-                            <input type="radio" id="Radio3">
+                        <div class="RadioList absolute top-2 right-2 flex flex-row gap-x-2">
+                            <div class="w-3 h-3 bg-white rounded-full"></div>
+                            <div class="w-3 h-3 bg-white rounded-full"></div>
+                            <div class="w-3 h-3 bg-white rounded-full"></div>
                         </div>
                     </div>
                 </div>
@@ -89,55 +89,55 @@
                 </div>
                 
                 <!-- Third Row -->
-                <div class="grid grid-cols-10 w-full gap-x-6">  
+                <div class="grid grid-cols-10 w-full gap-x-6 grid-rows-2 gap-y-2">  
                     <!-- Transaction Container -->
-                    <div class="grid col-span-7 bg-white drop-shadow-subs-card py-2 px-8 rounded-md w-full">
+                    <div class="grid col-span-7 bg-white drop-shadow-subs-card py-2 px-8 rounded-md w-full row-span-2">
                         <h1 class="text-2xl pt-2" >Latest Transaction</h1>
                         <div class="w-[100%] py-2 flex flex-col gap-y-4">
                             <hr class="h-[3px] border-0 w-[5%] bg-black">
                         </div>
                         <div class="flex flex-col w-full">
-                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-200">
-                                <h1 class="col-span-1 font-bold text-lg">17-08-1945</h1>
+                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-300">
+                                <h1 class="col-span-1 font-bold text-md">17-08-1945</h1>
                                 <h1 class="col-span-2">Beliin Rifian Starbucks Expresso  </h1>
                                 <h1 class="col-span-1 font-bold text-md">Food & Beverage</h1>
-                                <h1 class="col-span-1 font-bold text-lg text-red-600">- Rp 200.000</h1>
+                                <h1 class="col-span-1 font-bold text-md text-red-600">- Rp 200.000</h1>
                             </div>
-                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-200">
-                                <h1 class="col-span-1 font-bold text-lg">17-08-1945</h1>
+                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-300">
+                                <h1 class="col-span-1 font-bold text-md">17-08-1945</h1>
                                 <h1 class="col-span-2">Beliin Rifian Starbucks Expresso </h1>
                                 <h1 class="col-span-1 font-bold text-md">Food & Beverage</h1>
-                                <h1 class="col-span-1 font-bold text-lg text-red-600">- Rp 200.000</h1>
+                                <h1 class="col-span-1 font-bold text-md text-red-600">- Rp 200.000</h1>
                             </div>
-                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-200">
-                                <h1 class="col-span-1 font-bold text-lg">17-08-1945</h1>
+                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-300">
+                                <h1 class="col-span-1 font-bold text-md">17-08-1945</h1>
                                 <h1 class="col-span-2">Beliin Rifian Starbucks Expresso </h1>
                                 <h1 class="col-span-1 font-bold text-md">Food & Beverage</h1>
-                                <h1 class="col-span-1 font-bold text-lg text-red-600">- Rp 200.000</h1>
+                                <h1 class="col-span-1 font-bold text-md text-red-600">- Rp 200.000</h1>
                             </div>
-                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-200">
-                                <h1 class="col-span-1 font-bold text-lg">17-08-1945</h1>
+                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-300">
+                                <h1 class="col-span-1 font-bold text-md">17-08-1945</h1>
                                 <h1 class="col-span-2">Beliin Rifian Starbucks Expresso </h1>
                                 <h1 class="col-span-1 font-bold text-md">Food & Beverage</h1>
-                                <h1 class="col-span-1 font-bold text-lg text-red-600">- Rp 200.000</h1>
+                                <h1 class="col-span-1 font-bold text-md text-red-600">- Rp 200.000</h1>
                             </div>
-                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-200">
-                                <h1 class="col-span-1 font-bold text-lg">17-08-1945</h1>
+                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-300">
+                                <h1 class="col-span-1 font-bold text-md">17-08-1945</h1>
                                 <h1 class="col-span-2">Beliin Rifian Starbucks Expresso </h1>
                                 <h1 class="col-span-1 font-bold text-md">Food & Beverage</h1>
-                                <h1 class="col-span-1 font-bold text-lg text-red-600">- Rp 200.000</h1>
+                                <h1 class="col-span-1 font-bold text-md text-red-600">- Rp 200.000</h1>
                             </div>
-                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-200">
-                                <h1 class="col-span-1 font-bold text-lg">17-08-1945</h1>
+                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-300">
+                                <h1 class="col-span-1 font-bold text-md">17-08-1945</h1>
                                 <h1 class="col-span-2">Beliin Rifian Starbucks Expresso </h1>
                                 <h1 class="col-span-1 font-bold text-md">Food & Beverage</h1>
-                                <h1 class="col-span-1 font-bold text-lg text-red-600">- Rp 200.000</h1>
+                                <h1 class="col-span-1 font-bold text-md text-red-600">- Rp 200.000</h1>
                             </div>
-                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-200">
-                                <h1 class="col-span-1 font-bold text-lg">17-08-1945</h1>
+                            <div class="grid grid-cols-5 py-3 gap-x-4 border-b-2 border-gray-300">
+                                <h1 class="col-span-1 font-bold text-md">17-08-1945</h1>
                                 <h1 class="col-span-2">Beliin Rifian Starbucks Expresso </h1>
                                 <h1 class="col-span-1 font-bold text-md">Food & Beverage</h1>
-                                <h1 class="col-span-1 font-bold text-lg text-red-600">- Rp 200.000</h1>
+                                <h1 class="col-span-1 font-bold text-md text-red-600">- Rp 200.000</h1>
                             </div>
                         </div>
                         
@@ -147,19 +147,28 @@
                         </div>
                     </div>
 
-                    <!-- Content Samting -->
-                    <div class="col-span-3 bg-white drop-shadow-subs-card py-2 px-6 rounded-md w-full flex flex-col items-center">
+                    <!-- Content Samting
+                    <div class="col-span-3 bg-white drop-shadow-subs-card py-2 px-6 rounded-md w-full flex flex-col">
 
-                        <h1 class="text-xl pt-2" >Top Spending This Month</h1>
-                        <div class="w-[100%] py-2 flex flex-col gap-y-4 justify-center items-center">
-                            <hr class="h-[3px] border-0 w-[20%] bg-black">
+                        <h1 class="text-xl py-1" >This Month's Budget</h1>
+                        <div class="w-[100%] py-2">
+                            <hr class="h-[3px] border-0 w-[12%] bg-black">
                         </div>
 
-                        <!-- Category -->
-                        <div class="flex flex-col w-full py-3">
-                            <div class="flex flex-row justify-center">
-                                <div class="flex flex-col">
-                                    <div class="flex flex-row items-center py-5 border-b-2 border-gray-200">
+                        Category
+                        <div class="flex flex-col w-full py-1">
+                            <div class="flex flex-row">
+                                <div class="flex flex-col w-full gap-y-5">
+                                    <div class="flex flex-row py-2 px-3 border-2 border-gray-300 rounded-md ">
+                                        <div class=" bg-fin-blue justify-center items-center p-3 rounded-full">
+                                            <img class="w-[30px] h-[30px] text-white p-[3px]" src="{{ asset('assets/dashboard/Bill.png') }}">
+                                        </div>
+                                        <div>
+                                            <h1 class="font-bold text-lg ml-2">Budget's Left</h1>
+                                            <h1 class="col-span-2 text-xl ml-2">Rp. 1.000.000</h1>
+                                        </div>
+                                    </div>
+                                    <div class="flex flex-row py-2 px-3 border-2 border-gray-300 rounded-md">
                                         <div class=" bg-fin-blue justify-center items-center p-3 rounded-full">
                                             <img class="w-[30px] h-[30px] text-white p-[3px]" src="{{ asset('assets/dashboard/Bill.png') }}">
                                         </div>
@@ -168,7 +177,7 @@
                                             <h1 class="col-span-2 text-xl ml-2">Rp. 1.000.000</h1>
                                         </div>
                                     </div>
-                                    <div class="flex flex-row items-center py-5 border-b-2 border-gray-200">
+                                    <div class="flex flex-row py-2 px-3 border-2 border-gray-300 rounded-md">
                                         <div class=" bg-fin-blue justify-center items-center p-3 rounded-full">
                                             <img class="w-[30px] h-[30px] text-white p-[3px]" src="{{ asset('assets/dashboard/Bill.png') }}">
                                         </div>
@@ -176,67 +185,107 @@
                                             <h1 class="font-bold text-lg ml-2">Food and Beverage</h1>
                                             <h1 class="col-span-2 text-xl ml-2">Rp. 1.000.000</h1>
                                         </div>
+                                    </div>   
+                                </div>
+                            </div>
+                        </div> -->
+                        
+                        <!-- Budget  -->
+                        <div class="col-span-3 bg-white drop-shadow-subs-card py-3 px-6 rounded-md w-full flex flex-col row-span-1">
+                            <h1 class="text-xl">This Month's Budget</h1>
+                            <div class="w-[100%] py-2">
+                                <hr class="h-[3px] border-0 w-[20%] bg-black">
+                            </div>
+                            <div class="flex flex-col w-full">
+                                <div class="flex flex-row">
+                                    <div class="flex flex-col w-full gap-y-2">
+                                        <div class="flex flex-row rounded-md py-2">
+                                            <div class=" bg-fin-blue flex justify-center items-center rounded-full px-2 py-1">
+                                                <img class="w-[40px] h-[32px]" src="{{ asset('assets/dashboard/Budget.png') }}">
+                                            </div>
+                                            
+                                            <div class="ml-2">
+                                                
+                                                <h1 class="font-bold text-md">Budget's Left</h1>
+                                                <h1 class="text-xl font-castoro">Rp. 1.000.000</h1>
+                                            </div>
+                                        </div>
+
+                                        <a href="#" class="text-sm border-2 bg-fin-blue text-center text-white py-3 rounded-md hover:border-fin-blue hover:bg-gray-100 hover:text-fin-blue duration-500 ease-in-out">
+                                            Set Budget
+                                        </a>
+
+                                        <p class="text-xs text-gray-500 py-2">
+                                        <Span class="font-bold">Reminder :</Span> 
+                                        Spend no more than 
+                                        <Span class="font-castoro font-bold">Rp 100.000</Span> 
+                                        each day so you can keep on budget for this month
+                                        </p>
+                                        
                                     </div>
-                                    <div class="flex flex-row items-center py-5 border-b-2 border-gray-200">
-                                        <div class=" bg-fin-blue justify-center items-center p-3 rounded-full">
-                                            <img class="w-[30px] h-[30px] text-white p-[3px]" src="{{ asset('assets/dashboard/Bill.png') }}">
-                                        </div>
-                                        <div>
-                                            <h1 class="font-bold text-lg ml-2">Food and Beverage</h1>
-                                            <h1 class="col-span-2 text-xl ml-2">Rp. 1.000.000</h1>
-                                        </div>
-                                    </div>    
                                 </div>
                             </div>
                         </div>
-                    </div>
+
+                        <!-- Budget  -->
+                        <div class="col-span-3 bg-white drop-shadow-subs-card py-3 px-6 rounded-md w-full flex flex-col row-span-1">
+                            <h1 class="text-xl">Top Spending This Month</h1>
+                            <div class="w-[100%] py-2">
+                                <hr class="h-[3px] border-0 w-[20%] bg-black">
+                            </div>
+                            <div class="flex flex-col gap-y-3 py-2">
+                                <div class="flex flex-col w-full">
+                                    <div class="flex flex-row py-2 rounded-md ">
+                                        <div class=" bg-fin-blue justify-center items-center p-3 rounded-full">
+                                            <img class="w-[25px] h-[25px] text-white p-[3px]" src="{{ asset('assets/dashboard/Bill.png') }}">
+                                        </div>
+                                        <div>
+                                            <h1 class="font-bold text-lg ml-2">Rp. 1.000.000</h1>
+                                            <h1 class="text-md ml-2 text-gray-500">Food & Beverage</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-col w-full">
+                                    <div class="flex flex-row py-2 rounded-md ">
+                                        <div class=" bg-fin-blue justify-center items-center p-3 rounded-full">
+                                            <img class="w-[25px] h-[25px] text-white p-[3px]" src="{{ asset('assets/dashboard/Bill.png') }}">
+                                        </div>
+                                        <div>
+                                            <h1 class="font-bold text-lg ml-2">Rp. 1.000.000</h1>
+                                            <h1 class="text-md ml-2 text-gray-500">Food & Beverage</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                 </div>
                 
             </div>
         </div>
         
-        <script>
+    <script>
+    // JavaScript for the slider
+    var Images = document.querySelectorAll('.ImageSlider img');
+    var RadioBtns = document.querySelectorAll('.RadioList div');
+    var currentIndex = 0;
+    
+    function Slide() {
+        Images[currentIndex].style.display= 'flex';
+        RadioBtns[currentIndex].style.backgroundColor = '#23273C';
+        setInterval(next, 1000);
+    }
 
-            const Images = getImage.querySelectorAll('#image-slide img');
+    function next() {
+        Images[currentIndex].style.display= 'none';
+        RadioBtns[currentIndex].style.backgroundColor = '#F7F7FA';
+        currentIndex = (currentIndex + 1) % Images.length;
+        Images[currentIndex].style.display= 'flex';
+        RadioBtns[currentIndex].style.backgroundColor = '#23273C';
+    }
 
-            const Radios = document.querySelectorAll('#radio-list input[type="radio"]');
+    window.onload = Slide();
 
-            const ImageLength = Images.length;
-            const RadioLength = Radios.length;
-            let ArrayRadio[]
-            let selectedIndex = -1;
-
-            function ImageSlider(){
-                Images.forEach(function(image) {
-                    image.style.display = 'none';
-                }); 
-
-                Radios.forEach(function(radio, index) {
-                    if (radio.checked) {
-                        selectedIndex = index;
-                    }
-                });
-
-                Radios.forEach(function(radio) {
-                    radio.checked = false;
-                });
-                
-                const specificImage = images[selectedIndex];    
-                specificImage.style.display = 'block';
-            
-            }
-
-            let counter = 1;
-
-            setInterval(function(){
-                document.getElementById('Radio' + counter).checked = true;
-                counter++;
-                if(counter>3){
-                    counter = 1;
-                }
-            },1000);
-
-        </script>
+    </script>
 
     </body>
 
