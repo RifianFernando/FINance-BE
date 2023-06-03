13c7c32a854bfafa875d94af59b61fec34b2c7fb
Rifian Fernando <rifianfernando19@gmail.com>
2023-06-03 16:50:33 +0700

    fix: fixing month transaction

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\TransactionJoinTable;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,12 +14,14 @@ class DashboardController extends Controller
         //get income or expense with query builder
         $transaction = TransactionJoinTable::where('user_id', auth()->user()->id)->get();
         $data = 0;
+        $currentMonth = Carbon::now()->format('m');
         if (isset($transaction[0])) {
             $data
                 = DB::table('transaction_join_tables AS tj')
                 ->leftJoin('transactions AS t', 'tj.transaction_id', '=', 't.id')
                 ->where('tj.user_id', $transaction[0]->user_id)
                 ->where('t.is_expense', $is_expense)
+                ->whereMonth('t.created_at', '=', $currentMonth)
                 ->selectRaw('SUM(t.amount) as amount')
                 ->first();
             $data = $data->amount;
@@ -26,6 +29,7 @@ class DashboardController extends Controller
 
         return $data;
     }
+
     public function index()
     {
         //get first name
